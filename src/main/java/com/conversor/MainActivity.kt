76a5d0df3dd2391360bc83f277e0d93a6e23b0f9
Felipe Matos <felipematosapps@gmail.com>
2024-07-19 17:52:11 -0300

    segundo commit

--- src/main/java/com/conversor/MainActivity.kt
+++ src/main/java/com/conversor/MainActivity.kt
@@ -5,10 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.conversor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnConversor.setOnClickListener {
+            val euros = binding.editEuro.text.toString().toDouble()
+            val dolares = euros * 1.08
+
+            binding.textResultado.text = dolares.toString()
+        }
     }
 }
